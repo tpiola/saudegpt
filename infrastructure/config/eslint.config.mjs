aaa728c991c8cdd51656472b65ea0b6dbadc4d5a
thiago_piola <thiagoopiola@gmail.com>
2026-06-09 23:47:15 -0300

    remove plugin react quebrado do lint

--- infrastructure/config/eslint.config.mjs
+++ infrastructure/config/eslint.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
   ...nextTs,
   globalIgnores([
     ".next/**",
@@ -11,26 +9,13 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
     "e2e/**",
+    "infrastructure/**",
     "playwright-report/**",
     "test-results/**",
   ]),
 ]);
 
 for (const cfg of eslintConfig) {
-  if (cfg.name === "next") {
-    cfg.rules = {
-      ...cfg.rules,
-      "react/display-name": "off",
-      "react/no-unescaped-entities": "warn",
-      "react-hooks/rules-of-hooks": "warn",
-    };
-  }
-  if (cfg.name === "next/core-web-vitals") {
-    cfg.rules = {
-      ...cfg.rules,
-      "@next/next/no-html-link-for-pages": "off",
-    };
-  }
   if (cfg.plugins && cfg.plugins["@typescript-eslint"]) {
     cfg.rules = {
       ...cfg.rules,
